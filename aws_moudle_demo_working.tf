8900400c4d554d2fc3e6f85e691507ca659f2c32
yaragalasuresh <106433319+yaragalasuresh@users.noreply.github.com>
2022-08-04 13:23:23 +0530

    Update aws_moudle_demo_working.tf

--- aws_moudle_demo_working.tf
+++ aws_moudle_demo_working.tf
@@ -13,7 +13,7 @@ provider "aws" {
   access_key = "AKIAQOLZS3A77TJGBDHR"
   secret_key = "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"
   region     = "us-east-1"
-}
+  
   default_tags {
     tags = {
       hashicorp-learn = "module-use"
