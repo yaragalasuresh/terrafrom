551104d08620e23ef8565a90c8823cb11f951902
yaragalasuresh <106433319+yaragalasuresh@users.noreply.github.com>
2022-08-04 13:26:31 +0530

    Update aws_moudle_demo_working.tf

--- aws_moudle_demo_working.tf
+++ aws_moudle_demo_working.tf
@@ -13,6 +13,7 @@ provider "aws" {
   access_key = "AKIAQOLZS3A77TJGBDHR"
   secret_key = "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"
   region     = "us-east-1"
+  default     = true
   
   default_tags {
     tags = {
